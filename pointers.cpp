8f88175104b82103d82a3c027b4cd418426094a9
MightyRaptorWarrior <150849435+MightyRaptorWarrior@users.noreply.github.com>
2026-09-24 00:59:00 +0000

    Add pointer-based min/max example

--- pointers.cpp
+++ pointers.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 using namespace std;
-double doubleValue(double *a);
+
+void highestNumber(double numbers[]);
+void lowestNumber(double numbers[]);
+void updateMin(double *number, double curr);
+void updateMax(double *number, double curr);
+void getInput();
+const int LIMIT_INPUT = 5;
+double numbers[LIMIT_INPUT];
+
+
+
 int main(){
 
-    double a = 5;
-    cout << doubleValue(&a) << endl;
+    getInput();
+    highestNumber(numbers);
+    lowestNumber(numbers);
+
+    return 0;
 }
-double doubleValue(double *a){
-    double value;
-    value = *a;
-    return value * 2;
+void getInput(){
+    double input =0;
+    for(int i=0; i < LIMIT_INPUT; i++){
+        cout << "Enter a number: ";
+        cin >> input;
+        cout << '\n';
+        numbers[i] = input;
+    }
+     
+}
+
+void highestNumber(double numbers[]){
+    double num = numbers[0];
+    for(int i=1; i<LIMIT_INPUT; i++){
+        updateMax(&num, numbers[i]);
+    }
+    cout << "the highest number is : " << num << endl;
+}
+void lowestNumber(double numbers[]){
+    double num = numbers[0];
+    for(int i=1; i<LIMIT_INPUT; i++){
+        updateMin(&num, numbers[i]);
+    }
+    cout << "the lowest number is : " << num << endl;
+}
+void updateMin(double *number, double curr){
+    if(curr < *number){
+        *number = curr;
+    }
+}
+void updateMax(double *number, double curr){
+    if(curr > *number){
+        *number = curr;
+    }
 }
